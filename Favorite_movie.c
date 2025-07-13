b4a5965241dd2153090c2c2aa19bba369b808141
pavanbhat-9 <pavan.bhat@s.amity.edu>
2025-07-13 21:19:08 +0530

    Genere added

--- Favorite_movie.c
+++ Favorite_movie.c
@@ -4,6 +4,7 @@ int main() {
     printf("Movie Title: Interstellar\n");
     printf("Release year:2014\n");
     printf("Director: Cristopher Nolan\n");
+    printf("Genere:Sci-Fi,Adeventure,Drama\n");
 
     return 0;
 }
